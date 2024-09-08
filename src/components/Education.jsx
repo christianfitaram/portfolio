38ef89a432e0898f95687593aad796f7a515e082
Christian Enrique Fita Ramirez <cefita@alumnat.copernic.cat>
2024-09-08 18:25:53 +0200

    Add : new certifications and projects

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
 import mongodblogo from "../assets/img/mongodbinc_logo.jpeg";
+import azurebadge from "../assets/img/azure_badge.png";
 import Button from 'react-bootstrap/Button';
 import { Container, Row, Col } from "react-bootstrap";
 export const Education = () => {
@@ -18,21 +19,21 @@ export const Education = () => {
                                         Degree in Development of Multiplatform Applications.{' '}
                                     </p>
                                     <div className="blockquote-footer">
-                                        Institut Nicolau Copèrnic <cite title="Source Title">(09/2023 - currently enrolled)</cite>
+                                        Institut Nicolau Copèrnic <cite title="">(09/2023 - currently enrolled)</cite>
                                     </div>
                                     <p>
                                         {' '}
                                         .NET Framework & C#{' '}
                                     </p>
                                     <div className="blockquote-footer">
-                                        EnfocaT Bootcamp <cite title="Source Title">(05/2024 - currently enrolled)</cite>
+                                        EnfocaT Bootcamp <cite title="">(05/2024 - currently enrolled)</cite>
                                     </div>
                                     <p>
                                         {' '}
                                         Fullstack web developer{' '}
                                     </p>
                                     <div className="blockquote-footer">
-                                        Codeable <cite title="Source Title">(Graduated in 08/2023)</cite>
+                                        Codeable <cite title="">(Graduated in 08/2023)</cite>
                                     </div>
                                 </blockquote>
                             </Card.Body>
@@ -43,7 +44,7 @@ export const Education = () => {
                                 <blockquote className="blockquote mb-0">
                                     <div className="wrapper" >
                                         <div className="img-col">
-                                            <img src={mongodblogo} alt='java' />
+                                            <img src={mongodblogo} alt='mongodb logo' />
                                         </div>
                                         <div className="info-col">
                                             <p>
@@ -52,13 +53,54 @@ export const Education = () => {
                                                 {''}
                                             </p>
                                             <div className="blockquote-footer">
-                                                MongoDB <cite title="Source Title">(Issue Date: 2024-06-11)</cite>
+                                                MongoDB <cite title="">(Issue Date: 2024-06-11)</cite>
                                             </div>
                                             <div className="blockquote-footer">
                                                 Certification ID: <cite title="Source Title">MDBs334b1eoxu</cite>
                                             </div>
                                             <Button variant="outline-light"  onClick={() => window.open(
                                             "https://learn.mongodb.com/c/ZNlzvcCPT4qUCbzxJzYkzA", "_blank")}>See Certification</Button>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                                <blockquote className="blockquote mb-0">
+                                    <div className="wrapper" >
+                                        <div className="img-col">
+                                            <img src={mongodblogo} alt='mongodb logo' />
+                                        </div>
+                                        <div className="info-col">
+                                            <p>
+                                                {''}
+                                                MongoDB Data Modeling Path
+                                                {''}
+                                            </p>
+                                            <div className="blockquote-footer">
+                                                MongoDB <cite title="">(Issue Date: 2024-06-11)</cite>
+                                            </div>
+                                            <div className="blockquote-footer">
+                                                Certification ID: <cite title="">MDBy0i72vcmkp</cite>
+                                            </div>
+                                            <Button variant="outline-light"  onClick={() => window.open(
+                                            "https://learn.mongodb.com/c/wwFMePmATf64iftNY1FoqA", "_blank")}>See Certification</Button>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                                <blockquote className="blockquote mb-0">
+                                    <div className="wrapper" >
+                                        <div className="img-col">
+                                            <img src={azurebadge} className="img-col" alt='microsoft azure insignia' width={200} height={200}/>
+                                        </div>
+                                        <div className="info-col">
+                                            <p>
+                                                {''}
+                                                Microsoft Certified: Azure Fundamentals
+                                                {''}
+                                            </p>
+                                            <div className="blockquote-footer">
+                                            Microsoft using Credly <cite title="">(Issue Date: 2024-07-11)</cite>
+                                            </div>
+                                            <Button variant="outline-light"  onClick={() => window.open(
+                                            "https://www.credly.com/badges/c14c74d9-eefe-4a79-93b9-118ccf813147/linked_in_profile", "_blank")}>See Certification</Button>
                                         </div>
                                     </div>
                                 </blockquote>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,8 @@ import jsicon from "../assets/img/javascript.svg";
 import reacticon from "../assets/img/react.svg";
 import csicon from "../assets/img/csharp.svg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
+import typescript from "../assets/img/typescript_logo.svg";
+import nextjs from "../assets/img/next-js.svg"
 import 'animate.css';
 import TrackVisibility from "react-on-screen";
 export const Projects = () => {
@@ -38,15 +40,18 @@ export const Projects = () => {
 
                             <Col xs={12} md={6} className='margin-bottom-20px'>
                                 <Card data-bs-theme="dark">
-                                    <Card.Header as="h5">Project #2<div className='social-icon iconSmall'><img src={javaicon} alt='java' /></div></Card.Header>
+                                    <Card.Header as="h5">Project #2
+                                    <div className='social-icon iconSmall'><img src={typescript} alt='typescript icon' /></div>
+                                    <div className='social-icon iconSmall'><img src={nextjs} alt='nextjs icon' /></div>
+                                    <div className='social-icon iconSmall'><img src={reacticon} alt='react icon' /></div>
+                                    </Card.Header>
                                     <Card.Body className='regularText'>
-                                        <Card.Title>Car shop</Card.Title>
+                                        <Card.Title>The Pokemon APP</Card.Title>
                                         <Card.Text className='width100 text-align-left'>
-                                            Also an assignment in the first months of school to use classes, objects, and arrays.
-                                            The task was to develop a console-running application for a car shop, on which it is possible to store data on cars, issues, sells, etc.
+                                        A single page web application that displays an interactive list of pokemons which it retrieves from an external public API and it is also possible to see the individual details of each pokemon. It was made using NextJS, React and TypeScript.
                                         </Card.Text>
                                         <Button variant="primary" onClick={() => window.open(
-                                            "https://github.com/christianfitaram?tab=repositories", "_blank")}>Go to repository</Button>
+                                            "https://github.com/christianfitaram/the-pokemon-app", "_blank")}>Go to repository</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
@@ -63,7 +68,7 @@ export const Projects = () => {
                                             It stores in memory the name and score of a participant if they want it to.
                                         </Card.Text>
                                         <Button variant="primary" onClick={() => window.open(
-                                            "https://github.com/christianfitaram?tab=repositories", "_blank")}>Go to repository</Button>
+                                            "https://github.com/christianfitaram/clivia", "_blank")}>Go to repository</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
@@ -76,11 +81,12 @@ export const Projects = () => {
                                     <Card.Body className='regularText'>
                                         <Card.Title>Portfolio</Card.Title>
                                         <Card.Text className='width100 text-align-left'>
-                                            My portfolio was made in react, react-bootstrap, animate.css, nodemailer, and express.
-                                            I got inspiration from the philosophy of "less design is the better design" and the said components' official websites.
+                                            My portfolio was made using react, react-bootstrap, animate.css, nodemailer, and express.
+                                            I got inspiration from the philosophy of "less design is the better design and the said components' official websites.
+                                            <br/>
                                         </Card.Text>
                                         <Button variant="primary" onClick={() => window.open(
-                                            "https://github.com/christianfitaram?tab=repositories", "_blank")}>Go to repository</Button>
+                                            "https://github.com/christianfitaram/portfolio", "_blank")}>Go to repository</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
